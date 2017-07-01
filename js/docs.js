679072e9e48b0685d4ca268c05abb77c1a6bf09b
Leah Sherrell <leahsherrell@icloud.com>
2017-07-01 16:15:38 -0700

    create DoctorOptions constructor function

--- js/docs.js
+++ js/docs.js
@@ -1,7 +1,11 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+function DocOptions () {
+
+}
+
+DocOptions.prototype.getDoctors = function(condition) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ condition+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       console.log(result);
     })
@@ -10,4 +14,4 @@ exports.getDoctors = function(medicalIssue) {
     });
 };
 
-exports.doctorModule = Doctor;
+exports.docOptionsModule = DocOptions;
