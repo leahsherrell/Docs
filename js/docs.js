55eb03fb1b4348b49b4e1dedc064e163461c39ef
Leah Sherrell <leahsherrell@icloud.com>
2017-07-01 17:51:08 -0700

    target first name of doc to test displaying specific data for a search

--- js/docs.js
+++ js/docs.js
@@ -8,11 +8,11 @@ function DocOptions () {
 DocOptions.prototype.getDoctors = function(medicalIssue, apiKey) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-
+     $('#doctorSearch').append('<li>' + result.data[0].profile.first_name + '</li>');
      console.log(result);
     })
    .fail(function(error){
-      console.log("fail");
+      alert("Sorry, the search was unsuccessful.");
     });
 };
 
